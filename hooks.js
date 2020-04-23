7e6573750534d1aeb6ce276a4079ccb898f50514
Stefan Mueller <mu.stefan@googlemail.com>
2020-04-23 23:13:43 +0200

    Use editorContainerBox for etherpad > 1.8.1

--- hooks.js
+++ hooks.js
@@ -5,8 +5,13 @@ exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
   return cb();
 }
 
-exports.eejsBlock_body = function (hook_name, args, cb) {
+exports.eejsBlock_editorContainerBox = function (hook_name, args, cb) {
   args.content = args.content + eejs.require("ep_bookmark/templates/modals.ejs", {}, module);
+  return cb();
+}
+
+exports.eejsBlock_body = function (hook_name, args, cb) {
+  args.content = args.content + eejs.require("ep_bookmark/templates/modals.old.ejs", {}, module);
   return cb();
 }
 
